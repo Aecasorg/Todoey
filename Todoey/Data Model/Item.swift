12da404f81c7c14a10698a5a7558f2a715de9aa7
Henrik Gustavii <henrik@gustavii.com>
2018-04-11 08:01:12 +0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Henrik Gustavii on 10/04/2018.
+//  Copyright © 2018 Aecasorg. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
